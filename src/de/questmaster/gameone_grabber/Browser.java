ef6846846b97378b1d86a114738e37f620a43715
Daniel Jacobi <jacobi@questmaster.de>
2011-01-20 00:24:03 +0100

    Minimal UI working next: param filters next: grabbing

--- src/de/questmaster/gameone_grabber/Browser.java
+++ src/de/questmaster/gameone_grabber/Browser.java
@@ -1,11 +1,11 @@
 package de.questmaster.gameone_grabber;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URL;
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,28 +15,66 @@ import java.net.URL;
  * To change this template use File | Settings | File Templates.
  */
 public class Browser extends JFrame {
-    private JEditorPane browserPane;
     private JPanel panel1;
-    private JTextField adressField;
-    private JButton getEpisodeButton;
+    private JTextField episodeField;
+    private JTextField saveField;
+    private JButton grabButton;
+    private JButton selectButton;
 
     public Browser() {
         super("GameOne Grabber");
-
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(panel1);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
-
-        adressField.addActionListener(new ActionListener() {
+        selectButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              */
             public void actionPerformed(ActionEvent e) {
-                try{
-                    browserPane.setPage(new URL(adressField.getText()));
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+                String curPath = saveField.getText();
+
+                JFileChooser chooser = new JFileChooser();
+                chooser.setSelectedFile(new File(curPath));
+                int returnVal = chooser.showSaveDialog(panel1);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    saveField.setText(chooser.getSelectedFile().getAbsolutePath());
                 }
+
+            }
+        });
+        episodeField.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             */
+            public void actionPerformed(ActionEvent e) {
+                if (episodeField.getText().startsWith("http://www.gameone.de/tv/") && episodeField.getText().length() > 26) {
+                    String episodeNumber = episodeField.getText().substring(25);
+
+                    int i = 0;
+                    while (i < episodeNumber.length() && Character.isDigit(episodeNumber.charAt(i))) {
+                        i++;
+                    }
+
+                    if (i > 0) {
+                        episodeNumber = episodeNumber.substring(0, i);
+                        saveField.setText(saveField.getText().replace("XXX", episodeNumber));
+                        grabButton.setEnabled(true);
+                        episodeField.setEnabled(false);
+                    }
+                }
+            }
+        });
+        grabButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             */
+            public void actionPerformed(ActionEvent e) {
+                saveField.setEnabled(false);
+                selectButton.setEnabled(false);
+
+                // TODO: start grabing
+
+                grabButton.setEnabled(false);
             }
         });
     }
--- src/de/questmaster/gameone_grabber/Main.java
+++ src/de/questmaster/gameone_grabber/Main.java
@@ -9,22 +9,15 @@
 package de.questmaster.gameone_grabber;
 
 
-import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Main {
 
     public static void main(String[] args) {
+
         // open main screen
         Browser b = new Browser();
-
-        b.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent evt) {
-                System.exit(0);
-            }
-        });
-        b.setSize(600, 800);
         b.setVisible(true);
     }
 }
