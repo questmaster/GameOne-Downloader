d52301adcdfaca1c935d669ca38ca83184d1cd96
Daniel Jacobi <daniel@questmaster.de>
2011-02-08 10:43:16 +0100

    added copyright notice

--- src/de/questmaster/gameone_downloader/Downloader.java
+++ src/de/questmaster/gameone_downloader/Downloader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Daniel Jacobi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package de.questmaster.gameone_downloader;
 
 import javax.swing.*;
@@ -126,9 +142,7 @@ public class Downloader extends JDialog implements Runnable {
         dumpOutput.append(MessageFormat.format(resBundle.getString("downloader.grabbing.data.from.url"), sUrl));
         try {
             Desktop.getDesktop().browse(new URI(sUrl));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -218,6 +232,8 @@ public class Downloader extends JDialog implements Runnable {
 
                 // call rtmpdump
                 try {
+                    System.err.println("rtmpdump.exe -r \"" + streamUrl + "\" -o " + dumpLocation + " -W \"" + embededSwf + "\" -p \""
+                            + sUrl + "\" -u \"" + magicWord + "\"");
                     ProcessBuilder pb = new ProcessBuilder(rtmpDumpLocation,
                             "-r", "\"" + streamUrl + "\"",
                             "-o", dumpLocation,
--- src/de/questmaster/gameone_downloader/Main.java
+++ src/de/questmaster/gameone_downloader/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Daniel Jacobi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 /**
  * Created by IntelliJ IDEA.
  * Date: 19.01.11
@@ -8,7 +24,29 @@
 package de.questmaster.gameone_downloader;
 
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JApplet {
+
+    private JApplet applet = this;
+
+    public void init() {
+        //Execute a job on the event-dispatching thread:
+        //creating this applet's GUI.
+
+        try {
+            javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    Browser b = new Browser();
+                    applet.add(b.getContentPane());
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't successfully complete");
+            e.printStackTrace(System.err);
+        }
+    }
 
     public static void main(String[] args) {
 
